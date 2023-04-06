c6a268ec8010556b0e75ba6c47b2b60250d695d6
kyongha <magpia001@gmail.com>
2023-04-06 16:39:14 +0900

    post 작성자 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 import os
 
@@ -13,8 +14,10 @@ class Post(models.Model):
   created_at = models.DateField(auto_now_add=True)
   updated_at = models.DateField(auto_now=True)
 
+  author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
   def __str__(self):
-    return f'[{self.id}] {self.title}'
+    return f'[{self.id}] {self.title} :: {self.author}'
   
   def get_absolute_url(self):
     return f'/blog/{self.pk}/'
