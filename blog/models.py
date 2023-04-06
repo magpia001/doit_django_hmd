da17c7b3e7d83d83da1b74169fd3b0daac849448
kyongha <magpia001@gmail.com>
2023-04-06 12:53:34 +0900

    포스트 목록이 하나도 없을경우 예외처리

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ import os
 # Create your models here.
 class Post(models.Model):
   title = models.CharField(max_length=30)
-
+  hook_text = models.CharField(max_length=100, blank=True)
   content = models.TextField()
   head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d', blank=True)
   file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
@@ -22,12 +22,12 @@ class Post(models.Model):
   # upload 파일 이름 가져오기
   def get_file_name(self):
     f_name = os.path.basename(self.file_upload.name)
-    print(f_name)
+    # print(f_name)
     return f_name
   
   # upload 파일 확장자 가져오기
   def get_file_ext(self):
     f_ext = self.get_file_name().split(".")[-1]
-    print(f_ext)
+    # print(f_ext)
     return f_ext
   
