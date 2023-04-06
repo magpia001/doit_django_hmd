744ec83596f953f5bf144be05405426c90048b4e
kyongha <magpia001@gmail.com>
2023-04-06 17:50:56 +0900

    category 데이터 모델 정의, admin 등록

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,17 @@ from django.contrib.auth.models import User
 import os
 
 # Create your models here.
+class Category(models.Model):
+  name = models.CharField(max_length=50, unique=True)
+  slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+  class Meta:
+    verbose_name_plural = '카테고리'
+
+  def __str__(self):
+    return self.name
+
+
 class Post(models.Model):
   title = models.CharField(max_length=30)
   hook_text = models.CharField(max_length=100, blank=True)
@@ -15,6 +26,11 @@ class Post(models.Model):
   updated_at = models.DateField(auto_now=True)
 
   author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+  category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+
+  class Meta:
+    verbose_name_plural = '블로그 포스트'
+
 
   def __str__(self):
     return f'[{self.id}] {self.title} :: {self.author}'
@@ -34,3 +50,5 @@ class Post(models.Model):
     # print(f_ext)
     return f_ext
   
+
+
