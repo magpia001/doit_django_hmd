3e22082b6cc32bd4d691f0dbf355eb0281332cf4
kyongha <magpia001@gmail.com>
2023-04-06 12:25:36 +0900

    첨부 파일 있을 경우 포스트 상세 페이지에서 다운로드 표시, 아이콘 표시

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+import os
+
 # Create your models here.
 class Post(models.Model):
   title = models.CharField(max_length=30)
+
   content = models.TextField()
   head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d', blank=True)
   file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
@@ -15,3 +18,16 @@ class Post(models.Model):
   
   def get_absolute_url(self):
     return f'/blog/{self.pk}/'
+  
+  # upload 파일 이름 가져오기
+  def get_file_name(self):
+    f_name = os.path.basename(self.file_upload.name)
+    print(f_name)
+    return f_name
+  
+  # upload 파일 확장자 가져오기
+  def get_file_ext(self):
+    f_ext = self.get_file_name().split(".")[-1]
+    print(f_ext)
+    return f_ext
+  
